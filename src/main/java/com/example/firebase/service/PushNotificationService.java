88790bf0e848c415b9c4c092c5ccabea8e1c43e1
Siva <sivasubrahmanyam.pagolu@techwave.net>
2022-12-14 18:29:15 +0530

    sonar issue fix

--- src/main/java/com/example/firebase/service/PushNotificationService.java
+++ src/main/java/com/example/firebase/service/PushNotificationService.java
@@ -10,10 +10,8 @@ import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.http.HttpResponse;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 @Service
 public class PushNotificationService {
@@ -23,12 +21,13 @@ public class PushNotificationService {
 
     @Value("${firebase.key}")
     private String key;
+
     public PushNotificationService(RestTemplateBuilder restTemplateBuilder) {
         this.restTemplate = restTemplateBuilder.build();
     }
 
 
-    public String sendNotification(String token, Notification notification){
+    public String sendNotification(String token, Notification notification) {
         HttpEntity<String> httpEntity = null;
         ResponseEntity<String> responseEntity = null;
         HttpHeaders httpHeaders = null;
@@ -38,43 +37,42 @@ public class PushNotificationService {
 
         httpHeaders = new HttpHeaders();
         httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-        httpHeaders.set(HttpHeaders.AUTHORIZATION,"key=" + key);
-        inputString = prepareNotificationContent(token,notification);
+        httpHeaders.set(HttpHeaders.AUTHORIZATION, "key=" + key);
+        inputString = prepareNotificationContent(token, notification);
         String url = "https://fcm.googleapis.com/fcm/send";
-        httpEntity = new HttpEntity<>(inputString,httpHeaders);
-        responseEntity = this.restTemplate.postForEntity(url,httpEntity,String.class);
+        httpEntity = new HttpEntity<>(inputString, httpHeaders);
+        responseEntity = this.restTemplate.postForEntity(url, httpEntity, String.class);
         httpStatus = responseEntity.getStatusCode();
-        if(httpStatus==HttpStatus.OK){
+        if (httpStatus == HttpStatus.OK) {
             response = responseEntity.getBody();
             logger.info(responseEntity.getBody());
             logger.info("success");
-        }
-        else{
+        } else {
             logger.info("failed");
         }
         return response;
     }
 
-    public String prepareNotificationContent(String token,Notification notification){
+    public String prepareNotificationContent(String token, Notification notification) {
         Map<String, Object> inputMap = null;
-        Map<String,Object>  notificationContentMap = null;
+        Map<String, Object> notificationContentMap = null;
         String inputString = null;
         String notificationTitle = null;
         String notificationBody = null;
 
         inputMap = new HashMap<>();
         notificationContentMap = new HashMap<>();
-        if(notification!=null){
+        if (notification != null) {
             notificationTitle = notification.getTitle();
             notificationBody = notification.getBody();
         }
-        notificationContentMap.put("title",notificationTitle);
-        notificationContentMap.put("body",notificationBody);
-        notificationContentMap.put("sound","default");
-        notificationContentMap.put("show_in_foreground",true);
-        inputMap.put("to",token);
-        inputMap.put("priority","high");
-        inputMap.put("notification",notification);
+        notificationContentMap.put("title", notificationTitle);
+        notificationContentMap.put("body", notificationBody);
+        notificationContentMap.put("sound", "default");
+        notificationContentMap.put("show_in_foreground", true);
+        inputMap.put("to", token);
+        inputMap.put("priority", "high");
+        inputMap.put("notification", notification);
         Gson gson = new Gson();
         inputString = gson.toJson(inputMap);
 
